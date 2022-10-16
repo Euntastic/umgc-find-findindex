d3ca7ea54f5459e7a072122e7b66dfc13266dc9c
Eun Lee <euntastic@gmail.com>
2022-10-16 15:30:37 -0400

    Finished exercise.

--- find-findindex.js
+++ find-findindex.js
@@ -1,5 +1,14 @@
+// ███████ ██ ███    ██ ██████      ██ ███████ ██ ███    ██ ██████  ██ ███    ██ ██████  ███████ ██   ██ 
+// ██      ██ ████   ██ ██   ██    ██  ██      ██ ████   ██ ██   ██ ██ ████   ██ ██   ██ ██       ██ ██  
+// █████   ██ ██ ██  ██ ██   ██   ██   █████   ██ ██ ██  ██ ██   ██ ██ ██ ██  ██ ██   ██ █████     ███   
+// ██      ██ ██  ██ ██ ██   ██  ██    ██      ██ ██  ██ ██ ██   ██ ██ ██  ██ ██ ██   ██ ██       ██ ██  
+// ██      ██ ██   ████ ██████  ██     ██      ██ ██   ████ ██████  ██ ██   ████ ██████  ███████ ██   ██ 
+
 /* 
-Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+Write a function called `findUserByUsername` which accepts an array of objects, 
+each with a key of username, and a string. The function should return the first 
+object with the key of username that matches the string passed to the function. 
+If the object is not found, return undefined. 
 
 const users = [
   {username: 'mlewis'},
@@ -12,10 +21,14 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(element => element['username'] == username);
+}
 
 /*
-Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+Write a function called `removeUser` which accepts an array of objects, each 
+with a key of username, and a string. The function should remove the object 
+from the array. If the object is not found, return undefined. 
 
 const users = [
   {username: 'mlewis'},
@@ -27,4 +40,9 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let userIndex = usersArray.findIndex(element => element['username'] == username);
+  console.log(userIndex);
+  if (userIndex == -1) return;
+  return usersArray.splice(userIndex, 1)[0];
+}
